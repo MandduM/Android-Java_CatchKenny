349482a96b5f2e17cb2b5c7f60699a092e7a891c
Ali Kaan Ekmekci <ekmekcialikaan@gmail.com>
2023-03-30 16:46:13 +0300

    Catch Kenny game complete

---
+++ app/src/main/java/com/ake/catchkenny/MainActivity.java
@@ -0,0 +1,112 @@
+package com.ake.catchkenny;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timeText,scoreText;
+    int scoree;
+    ImageView imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9;
+    ImageView[] imageArray;
+
+    Handler handler;
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeText = findViewById(R.id.timeText);
+        scoreText = findViewById(R.id.scoreText);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+        imageArray = new ImageView[] {imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9};
+
+        scoree = 0;
+
+        gorselSakla();
+
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long l) {
+                timeText.setText("TIME : "+ l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+                timeText.setText("TIME IS OVER");
+                handler.removeCallbacks(runnable);
+                for(ImageView image : imageArray){
+                    image.setVisibility(View.INVISIBLE);
+                }
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("AGAIN ?");
+                alert.setMessage("How About Starting Over ?");
+                alert.setPositiveButton("YES", (dialogInterface, i) -> {
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                });
+
+                alert.setNegativeButton("NO", (dialogInterface, i) -> Toast.makeText(MainActivity.this,"GAME OVER",Toast.LENGTH_LONG));
+
+                alert.show();
+            }
+        }.start();
+    }
+
+    public void Score(View view){
+
+        scoree++;
+        scoreText.setText("Score : "+scoree);
+    }
+
+    public void gorselSakla(){
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                for(ImageView image : imageArray){
+                    image.setVisibility(View.INVISIBLE);
+                }
+
+                Random random = new Random();
+                int i = random.nextInt(9);
+                imageArray[i].setVisibility(View.VISIBLE);
+
+                handler.postDelayed(this,500);
+            }
+        };
+
+        handler.post(runnable);
+
+
+    }
+}
